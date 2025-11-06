aa742825484e154e4df29dfebdc6dc13e5df269a
HarshG01001 <02harshg@gmail.com>
2025-11-07 01:09:08 +0530

    Fix: Listen on Render's dynamic process.env.PORT

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const cors = require('cors');
 
 // --- App Initialization ---
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 // --- Caching Variables ---
 let cachedData = null;    // This will hold our NASA event data
